d86b86baf04dda99ca2d886092c59d41f8337f2e
Benedict <z5457257@ad.unsw.edu.au>
2025-02-13 01:28:21 +1100

    binary tree works for calculator and two args only, think about how to perform def and defun operations

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "parser.h"
 
 #define TYPE_INT 0
 #define TYPE_OPR 1
@@ -12,6 +16,8 @@ struct node {
     struct node *right;
 };
 
+bool isOperator(char c);
+
 /*
     Given an array of tokens (strings),
     Converts into a binary AST.
@@ -19,8 +25,18 @@ struct node {
 */
 struct node *parse(char **tokens, int ntokens) {
     for (int i = 0; i < ntokens; ++i) {
-                
+        char *token = tokens[i];
+        if (strlen(token) == 1 && isOperator(token[0])) {
+            Node n = malloc(sizeof(*n));
+            n->type = TYPE_OPR;
+            n->val = token;
+        }
+
     }
-    return root;
+    return NULL;
 }
 
+// Check if character is an operator
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
